dc043efb90e3210dde7f2ad8035843635042f615
DenisKent <kentdenis6@hotmail.co.uk>
2018-10-01 08:28:37 +0100

    added favicon

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+var favicon = require('serve-favicon')
 
 const app = express();
 app.set('PORT', process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, '..', 'static')));
+app.use(favicon(path.join(__dirname,'..', 'static','images', 'favicon.ico')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
 });
